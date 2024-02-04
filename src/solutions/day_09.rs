84fde47ee3566ba931d1d037b1ff356bce957557
Exoodes <jakub.maca.01@gmail.com>
2024-02-04 23:21:09 +0100

    day_09

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -2,12 +2,45 @@ pub fn solve(input: &str) -> String {
     format!("{}\n{}\n", part_one(input), part_two(input))
 }
 
-pub fn part_one(_input: &str) -> u32 {
+pub fn part_one(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|s| {
+            s.split_ascii_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|l| compute_lines(l.clone()))
+        .map(|l| l.iter().map(|l| l.last().unwrap()).sum::<i64>())
+        .sum::<i64>()
+}
+
+
+pub fn part_two(_input: &str) -> i64 {
     0
 }
 
-pub fn part_two(_input: &str) -> u32 {
-    0
+fn compute_lines(initial_values: Vec<i64>) -> Vec<Vec<i64>> {
+    let mut lines = Vec::new();
+    lines.push(initial_values);
+
+    loop {
+        let last_row = lines.last().unwrap();
+        let mut new_row = Vec::new();
+
+        for i in 1..last_row.len() {
+            let value = last_row[i] - last_row[i - 1];
+            new_row.push(value);
+        }
+
+        if new_row.iter().all(|n| *n == 0) {
+            break;
+        }
+
+        lines.push(new_row);
+    }
+
+    lines
 }
 
 #[cfg(test)]
